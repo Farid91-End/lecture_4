bf9d97bf75aa897c5bb380226330a79665f70510
Faridunjon <farikjun.safarov@gmail.com>
2023-05-19 15:00:41 +0500

    Array

---
+++ main.js
@@ -0,0 +1,32 @@
+//task 1
+
+// function combineArrays(... arrays){
+//     return arrays.reduce((acc, curr) => acc.concat(curr), []);
+// }
+
+// let array1=[1,2,3];
+// let array2=[4,5,6];
+// let array3=[7,8,9];
+// console.log(combineArrays(array1, array2, array3));
+
+//task 2   .map
+// function incrementItems(arr){
+//     return arr.map(item => item + 1);
+// } console.log(incrementItems([1,2,3]));
+
+
+//task 3   
+// function getLastElement(arr){
+//     return arr[arr.length - 1];
+// } console.log(getLastElement([1,2,3, "Farid"]));
+
+//task 4
+
+// function findIndexOf(arr, item){
+//     return arr.indexOf(item);
+// }   
+// console.log(findIndexOf([1,"Al",3,"Farid",5], "Farid"));
+    
+//task5
+
+
